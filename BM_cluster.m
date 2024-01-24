ec129edfb4813bf7467ae27cea16648e586da188
Yubo02 <yubo2@illinois.edu>
2024-01-24 14:35:59 -0600

    update

--- BM_cluster.m
+++ BM_cluster.m
@@ -34,7 +34,8 @@ r = 2*K; % Rank of the matrix in BM
 beta = 10; % Coeffient of the augmented term
 alpha = 1e-6; % Step size
 tol = 1e-6; % Tolerance of stopping criteria
-maxiter = 50000; % Maximum of iterations
+tol_primal = 1e-3; % Tolerance of Primal update
+maxiter = 250000; % Maximum of iterations
 
 %% Projection operator
 proj = @(V) max(V,0)/norm(max(V,0),'fro') * sqrt(K) ;
@@ -50,31 +51,33 @@ gradAugL = @(U,y) -2*X'*(X*U) + ynew(U,y)*(one'*U) + one*(ynew(U,y)'*U);
 %% Implement algorithm
 
 y = zeros(n,1); % Intialization for dual variables
-U_0 = abs(randn(n,r)); U = sqrt(K)/norm(U_0,'fro')*U_0; % Random intialization
-
+U_0 = abs(randn(n,r)); U = proj(U_0); % Random intialization
+norm_one = norm(one); 
 
 for iter = 1:maxiter
 
     % Primal update
     G = gradAugL(U,y);
     Unew = proj(U - alpha*G);
-    
-    % Evaluate iterate
-    infeas = Unew*(Unew'*one) - one;
 
-    % Dual update
     rdiff = norm(Unew - U,'fro')/sqrt(K);
-    if rdiff < 1e-3
+
+    if rdiff < tol_primal % Tolerance of Primal update
+
+        % Evaluate iterate
+        infeas = Unew*(Unew'*one) - one;
+
+        % Stopping criteria    
+        if max(rdiff, norm(infeas)/norm_one)<tol
+           break
+        end
+
+        % Dual update
         U = Unew;
         y = y + beta*infeas;
+
     else
         U = Unew;
-    end
-
-
-    % Stopping criteria    
-    if max(rdiff, norm(infeas)/norm(one))<tol
-       break
     end
 
 
@@ -83,7 +86,6 @@ end
 % Output matrix
 
 U_out = U;
-
 end
 
 
